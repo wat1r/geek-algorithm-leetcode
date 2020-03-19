fd0fc036f1efcf15a87efba331c9272f8d6c6361
wat1r <cnwangzhou@hotmail.com>
2020-03-19 23:08:59 +0800

    Jump Game

---
+++ src/main/java/com/frankcooper/bank/_45.java
@@ -0,0 +1,20 @@
+package com.frankcooper.bank;
+
+public class _45 {
+
+
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= (i - j)) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
--- src/main/java/com/frankcooper/bank/_55.java
+++ src/main/java/com/frankcooper/bank/_55.java
@@ -98,4 +98,18 @@ public class _55 {
     }
 
 
+    /*
+
+     */
+    public boolean canJump4th(int[] nums) {
+        int n = nums.length;
+        int lastPos = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
 }
