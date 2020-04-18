4e6d2f446a726491a921df0fd055fcbf9aa01f41
wat1r <cnwangzhou@hotmail.com>
2020-04-19 00:25:48 +0800

    零钱兑换I

--- src/main/java/com/frankcooper/bank/_322.java
+++ src/main/java/com/frankcooper/bank/_322.java
@@ -1,12 +1,62 @@
 package com.frankcooper.bank;
 
+import java.util.HashMap;
+import java.util.*;
+
 //322. 零钱兑换 322. Coin Change
+//参见 背包思想解决零钱兑换问题.md
 public class _322 {
 
+    static _322 handler = new _322();
 
     public static void main(String[] args) {
-
+//        handler.coinChange1st(new int[]{1, 2, 5}, 11);
+        handler.coinChange2nd(new int[]{1, 2, 5}, 11);
     }
+
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int coinChange1st(int[] coins, int amount) {
+        return helper(coins, amount);
+    }
+
+    public int helper(int[] coins, int j) {
+        if (memo.containsKey(j)) return memo.get(j);
+        if (j == 0) return 0;
+        if (j < 0) return -1;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+            int tmp = helper(coins, j - coins[i]);
+            if (tmp == -1) continue;
+            res = Math.min(res, tmp + 1);
+        }
+        res = res == Integer.MAX_VALUE ? -1 : res;
+        memo.put(j, res);
+        return res;
+    }
+
+
+    public int coinChange2nd(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE;
+            if (j - coins[0] >= 0 && dp[0][j - coins[0]] != Integer.MAX_VALUE) {
+                dp[0][j] = dp[0][j - coins[0]] + 1;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int tmp = Integer.MAX_VALUE;
+                if (j - coins[i] >= 0 && dp[i][j - coins[i]] != Integer.MAX_VALUE) {
+                    tmp = dp[i][j - coins[i]] + 1;
+                }
+                dp[i][j] = Math.min(tmp, dp[i - 1][j]);
+            }
+        }
+        return dp[n - 1][amount] == Integer.MAX_VALUE ? -1 : dp[n - 1][amount];
+    }
+
 
     /*
         - `dp[i]`表示凑够总金额为`i`需要的最少的硬币的个数，因为要求得是`dp[amount]`，即凑够总金额为`amount`时所需要的最少硬币的个数，初始化`dp`时要`n+1`;
