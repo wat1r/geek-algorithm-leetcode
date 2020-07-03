b002e1018ca2e7412900cd8cf0adbf82fde72d93
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-07-03 09:09:43 +0800

    [tech-climbing-common] 1.74

---
+++ src/main/java/com/frankcooper/bank/_74.java
@@ -0,0 +1,21 @@
+package com.frankcooper.bank;
+
+/**
+ * @Date 2020/7/3
+ * @Author Frank Cooper
+ * @Description
+ */
+public class _74 {
+    static _74 handler = new _74();
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+
+        return false;
+    }
+}
---
+++ src/main/leetcode_manuscripts/binary_search/二分法之矩阵I[King Penguin].md
@@ -0,0 +1,7 @@
+## 二分法之矩阵I[King Penguin]
+
+
+
+先定下一个结论，二维矩阵被拍平后，
+
+![74_1](D:\Dev\SrcCode\geek-algorithm-leetcode\src\main\leetcode_manuscripts\binary_search\二分法之解矩阵I[].assets\74_1.jpg)
