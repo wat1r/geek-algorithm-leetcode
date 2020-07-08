24cc82e08e957fabbe8f6cea3a0e97c68ca0842e
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-07-08 21:15:14 +0800

    [tech-climbing-common] 1.113

--- src/main/java/com/frankcooper/struct/TreeNode.java
+++ src/main/java/com/frankcooper/struct/TreeNode.java
@@ -1,12 +1,13 @@
 package com.frankcooper.struct;
 
 public class TreeNode {
-    int val;
+    public int val;
     public TreeNode left;
     public TreeNode right;
 
     TreeNode(int x) {
         val = x;
     }
+
 
 }
