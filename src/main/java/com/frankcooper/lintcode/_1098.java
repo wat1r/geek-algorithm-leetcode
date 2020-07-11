36b2ed8897ff3c4be0e03ad56c24d85fbc4c6aa2
wat1r <cnwangzhou@hotmail.com>
2020-07-11 22:19:45 +0800

    _1098

--- src/main/java/com/frankcooper/lintcode/_1098.java
+++ src/main/java/com/frankcooper/lintcode/_1098.java
@@ -5,90 +5,88 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @Date 2020/7/10
- * @Author Frank Cooper
- * @Description
+ * Created by FrankCooper
+ * Date 2020/7/11 21:41
+ * Description
  */
 public class _1098 {
 
     static _1098 handler = new _1098();
 
     public static void main(String[] args) {
-//        handler.pathSumIV(new int[]{113, 215, 221});
-        handler.pathSumIV(new int[]{110, 211, 227, 318, 328, 331, 349, 431, 448, 457, 478, 489});
+        handler.pathSumIV(new int[]{113, 215, 221});
     }
 
 
     public int pathSumIV(int[] nums) {
-        Arrays.sort(nums);
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        Queue<Node> q = new LinkedList<>();
-        // 第一个元素入队
-        q.offer(new Node(1, getSeq(nums[0]), getVal(nums[0])));
+        if (nums == null || nums.length == 0) return 0;
+        Arrays.sort(nums);//sort,set the start number and got it
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(getLevel(nums[0]), getSeq(nums[0]), getValue(nums[0])));//add the root node
+        //i: 0...size-1
         int i = 1, size = nums.length;
-        int currLevel = 2;
-        int sum = 0;
+        int curLevel = 2; //the first level begin with 1 ,then start with curLevel
+        int res = 0;
         while (i < size) {
-            // 逐层遍历
-            while (i < size && getLevel(nums[i]) == currLevel) {
-                int num = nums[i];
-                //只把上一层无孩子的点计入sum
-                while (q.peek().seq != (getSeq(num) + 1) / 2) {// 举例：{110,211,227,318,328,331,349,431,448,457,478,489} 211->331
-                    if (q.peek().hasChild) {
-                        q.poll();
+            while (i < size && getLevel(nums[i]) == curLevel) {
+                int curNum = nums[i];
+                while (queue.peek().seq != (getSeq(curNum) + 1) / 2) {
+                    if (queue.peek().hasChild) {
+                        queue.poll();
                         continue;
                     }
-                    sum += q.poll().sum;
+                    res += queue.poll().value;
                 }
-                Node n = q.peek();
-                q.offer(new Node(currLevel, getSeq(num), n.sum + getVal(num)));
-                n.hasChild = true;
+                Node tmp = queue.peek();
+                queue.offer(new Node(curLevel, getSeq(curNum), tmp.value + getValue(curNum)));
+                tmp.hasChild = true;
                 i++;
             }
-            // q中还有可能残存上一层的点，把上一层清理干净
-            while (q.peek().level == currLevel - 1) {
-                if (q.peek().hasChild) {
-                    q.poll();
+            while (queue.peek().level == (curLevel - 1)) {
+                if (queue.peek().hasChild) {
+                    queue.poll();
                     continue;
                 }
-                sum += q.poll().sum;
+                res += queue.poll().value;
             }
-            currLevel++;
+            curLevel++;
         }
-        // 坑：最后q中还会残存最后一层的点，全部计入sum
-        while (!q.isEmpty()) {
-            sum += q.poll().sum;
+        while (!queue.isEmpty()) {
+            res += queue.poll().value;
         }
-        return sum;
+        return res;
     }
 
-    // 取左边第1位
+
+    //get level : 1st number
     private int getLevel(int num) {
         return num / 100;
     }
 
-    // 取左边第2位
+    //get seq : 2nd number
     private int getSeq(int num) {
         return (num % 100) / 10;
     }
 
-    // 取左边第3位
-    private int getVal(int num) {
+    //get value :3rd number
+    private int getValue(int num) {
         return num % 10;
     }
 
-    class Node {
+
+    static class Node {
+
         int level;
-        int sum;
         int seq;
+        int value;
         boolean hasChild;
 
-        public Node(int level, int seq, int sum) {
+        public Node(int level, int seq, int value) {
             this.level = level;
             this.seq = seq;
-            this.sum = sum;
+            this.value = value;
         }
     }
+
+
 }
