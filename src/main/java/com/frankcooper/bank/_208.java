2f7dc6fb635a594a2e63da0bd194437165ed3542
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-07-16 21:47:49 +0800

    [geek-algorithm-leetcode] 1.174

---
+++ src/main/java/com/frankcooper/bank/_208.java
@@ -0,0 +1,44 @@
+package com.frankcooper.bank;
+
+/**
+ * @Date 2020/7/16
+ * @Author Frank Cooper
+ * @Description
+ */
+public class _208 {
+
+
+    static class Trie {
+
+        /**
+         * Initialize your data structure here.
+         */
+        public Trie() {
+
+        }
+
+        /**
+         * Inserts a word into the trie.
+         */
+        public void insert(String word) {
+
+        }
+
+        /**
+         * Returns if the word is in the trie.
+         */
+        public boolean search(String word) {
+            return false;
+        }
+
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
+        public boolean startsWith(String prefix) {
+
+            return false;
+        }
+    }
+
+
+}
--- src/main/leetcode_manuscripts/design/数据结构设计Sumatran Rhinoceros.md
+++
@@ -1,2 +0,0 @@
-![Sumatran rhinoceros](D:\Dev\SrcCode\geek-algorithm-leetcode\src\main\leetcode_manuscripts\design\数据结构设计Sumatran Rhinoceros.assets\Sumatran rhinoceros.jpg)
-
