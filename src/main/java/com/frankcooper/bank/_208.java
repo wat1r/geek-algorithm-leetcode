14bf665875c4f4d36d64683d7237eb0f5f983262
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-07-22 22:16:36 +0800

    [geek-algorithm-leetcode] 1.208 前缀树

--- src/main/java/com/frankcooper/bank/_208.java
+++ src/main/java/com/frankcooper/bank/_208.java
@@ -1,5 +1,8 @@
 package com.frankcooper.bank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Date 2020/7/16
  * @Author Frank Cooper
@@ -8,17 +11,17 @@ package com.frankcooper.bank;
 public class _208 {
 
 
-    static class Trie {
+    static class Trie2 {
 
 
-        Trie[] children = new Trie[26];
+        Trie2[] next = new Trie2[26];
         boolean isEnd = false;
 
 
         /**
          * Initialize your data structure here.
          */
-        public Trie() {
+        public Trie2() {
 
         }
 
@@ -26,12 +29,12 @@ public class _208 {
          * Inserts a word into the trie.
          */
         public void insert(String word) {
-            Trie curr = this;
+            Trie2 curr = this;
             for (char c : word.toCharArray()) {
-                if (curr.children[c - 'a'] == null) {
-                    curr.children[c - 'a'] = new Trie();
+                if (curr.next[c - 'a'] == null) {
+                    curr.next[c - 'a'] = new Trie2();
                 }
-                curr = curr.children[c - 'a'];
+                curr = curr.next[c - 'a'];
             }
             curr.isEnd = true;
         }
@@ -40,10 +43,10 @@ public class _208 {
          * Returns if the word is in the trie.
          */
         public boolean search(String word) {
-            Trie curr = this;
+            Trie2 curr = this;
             for (char c : word.toCharArray()) {
-                if (curr.children[c - 'a'] == null) return false;
-                curr = curr.children[c - 'a'];
+                if (curr.next[c - 'a'] == null) return false;
+                curr = curr.next[c - 'a'];
             }
             return curr.isEnd;
         }
@@ -52,13 +55,61 @@ public class _208 {
          * Returns if there is any word in the trie that starts with the given prefix.
          */
         public boolean startsWith(String prefix) {
-            Trie curr = this;
+            Trie2 curr = this;
             for (char c : prefix.toCharArray()) {
-                if (curr.children[c - 'a'] == null) return false;
-                curr = curr.children[c - 'a'];
+                if (curr.next[c - 'a'] == null) return false;
+                curr = curr.next[c - 'a'];
             }
             return true;
         }
+    }
+
+
+    static class Trie {
+
+        class TrieNode {
+            Map<Character, TrieNode> next = new HashMap<>();
+            boolean isEnd = false;
+        }
+
+
+        TrieNode root = new TrieNode();
+
+        public Trie() {
+        }
+
+        public void insert(String word) {
+            TrieNode curr = root;
+            for (char c : word.toCharArray()) {
+                if (!curr.next.containsKey(c)) {
+                    TrieNode tmp = new TrieNode();
+                    curr.next.put(c, tmp);
+                    curr = tmp;
+                } else {
+                    curr = curr.next.get(c);
+                }
+            }
+            curr.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode curr = root;
+            for (char c : word.toCharArray()) {
+                if (!curr.next.containsKey(c)) return false;
+                curr = curr.next.get(c);
+            }
+            return curr.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode curr = root;
+            for (char c : prefix.toCharArray()) {
+                if (!curr.next.containsKey(c)) return false;
+                curr = curr.next.get(c);
+            }
+            return true;
+        }
+
     }
 
 
