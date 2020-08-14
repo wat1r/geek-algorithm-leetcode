50536e67bae252aadf878462e28b8d5262396eca
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-08-14 08:31:30 +0800

    [geek-algorithm-leetcode] 1.20

---
+++ src/main/java/com/frankcooper/bank/_20.java
@@ -0,0 +1,28 @@
+package com.frankcooper.bank;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @Date 2020/8/14
+ * @Author Frank Cooper
+ * @Description
+ */
+public class _20 {
+
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(']', '[');
+        map.put('}', '{');
+        map.put(')', '(');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '[' || c == '{' || c == '(') stack.push(c);
+            else if (!stack.isEmpty() && map.get(c) == stack.peek()) stack.pop();
+            else return false;
+        }
+        return stack.isEmpty();
+    }
+}
