dac0b66dd7233ebcce557ea9eb870d54b482cad3
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-08-21 17:22:13 +0800

    [geek-algorithm-leetcode] 1.课程表III

--- src/main/java/com/frankcooper/bank/_630.java
+++ src/main/java/com/frankcooper/bank/_630.java
@@ -16,7 +16,44 @@ public class _630 {
     }
 
 
-    public int scheduleCourse(int[][] courses) {
+    class _3rd {
+
+
+
+        public int scheduleCourse(int[][] courses) {
+
+
+            return 0;
+        }
+
+
+    }
+
+
+    class _2nd {
+        public int scheduleCourse(int[][] courses) {
+            Arrays.sort(courses, Comparator.comparing(a -> a[1]));
+            return backtrack(courses, 0, 0);
+        }
+
+        private int backtrack(int[][] courses, int duration, int index) {
+            if (index == courses.length) return 0;
+
+            int[] currCourse = courses[index];
+            int currDuration = currCourse[0];
+            int currDeadline = currCourse[1];
+            int choose = 0;
+            if (duration + currDuration <= currDeadline) {
+                choose = 1 + backtrack(courses, duration + currDuration, index + 1);
+            }
+            int notChoose = backtrack(courses, duration, index + 1);
+            return Math.max(choose, notChoose);
+        }
+
+    }
+
+
+    public int scheduleCourse1st(int[][] courses) {
         Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         int time = 0;
