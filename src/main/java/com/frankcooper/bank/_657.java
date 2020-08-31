67a690f62792cda9da25eda5fb226bd9c476c702
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-08-31 10:22:06 +0800

    [geek-algorithm-leetcode] 841

---
+++ src/main/java/com/frankcooper/bank/_657.java
@@ -0,0 +1,37 @@
+package com.frankcooper.bank;
+
+/**
+ * @Date 2020/8/28
+ * @Author Frank Cooper
+ * @Description
+ */
+public class _657 {
+
+
+    public boolean judgeCircle(String moves) {
+        if (moves == null | moves.length() == 0) return false;
+        int i = 0, j = 0;
+        for (char c : moves.toCharArray()) {
+            switch (c) {
+                case 'R':
+                    i++;
+                    break;
+                case 'L':
+                    i--;
+                    break;
+                case 'U':
+                    j--;
+                    break;
+                case 'D':
+                    j++;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        return i == 0 && j == 0;
+    }
+
+
+}
--- src/main/leetcode_manuscripts/dfs_bfs/map/图问题之地图分析[Apis Cerana].md
+++ src/main/leetcode_manuscripts/dfs_bfs/map/图问题之地图分析[Apis Cerana].md
@@ -56,7 +56,9 @@ public int maxDistance(int[][] grid) {
         }
         //全为0或者全为1的时候返回-1
         if (queue.isEmpty() || queue.size() == m * n) return -1;
+    	{% raw %}
         int[][] dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    	{% endrow %}
         int level = -1;
         while (!queue.isEmpty()) {
             int size = queue.size();
