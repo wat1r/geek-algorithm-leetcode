023f941b5b282e753e4580474ec6b6cb7a282380
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-09-15 09:58:29 +0800

    474

--- src/main/java/com/frankcooper/swordoffer/utils/PrintUtils.java
+++ src/main/java/com/frankcooper/swordoffer/utils/PrintUtils.java
@@ -17,4 +17,20 @@ public class PrintUtils {
         }
         System.out.println("--------------");
     }
+
+
+    public static void printMatrix(int[][][] matrix) {
+        int m = matrix.length, n = matrix[0].length, p = matrix[0][0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < p; ++k) {
+                    System.out.print(String.format("%3s", matrix[i][j][k]) + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("--------------");
+    }
+
+
 }
