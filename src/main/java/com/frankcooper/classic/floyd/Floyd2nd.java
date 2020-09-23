22c041b8f2c1239f6f9793414681740ab3ed72ff
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-09-23 08:50:57 +0800

    [geek-algorithm-leetcode] Floyd

--- src/main/java/com/frankcooper/classic/floyd/Floyd2nd.java
+++ src/main/java/com/frankcooper/classic/floyd/Floyd2nd.java
@@ -2,26 +2,74 @@ package com.frankcooper.classic.floyd;
 
 import com.frankcooper.swordoffer.utils.PrintUtils;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Floyd2nd {
 
 
+    class _2nd {
+//        public void testTwo() {
+//            init();
+//            Scanner sc = new Scanner(System.in);
+//            n = sc.nextInt();//顶点数量
+//            m = sc.nextInt();//边的条数
+//            int u, v, w;
+//            while (m-- > 0) {
+//                u = sc.nextInt();//from
+//                v = sc.nextInt();//to
+//                w = sc.nextInt();//weigh
+//                G[u][v] = G[v][u] = w;
+//            }
+////            floyd();
+//            System.out.println(G[1][n]);
+//        }
+
+
+//    strictfp
+
+//        static void floyd() {
+//            for (int k = 1; k <= n; k++) {
+//                for (int i = 1; i <= n; i++) {
+//                    for (int j = 1; j <= n; j++) {
+//                        G[i][j] = Math.min(G[i][j], G[i][k] + G[k][j]);
+//                    }
+//                }
+//            }
+//        }
+    }
+
+
     static class _1st {
 
-        static int N = 1005;
-        static int[][] G = new int[N][N];
+        class Edge {
+            private int u;
+            private int v;
+            private int w;
+
+            public Edge(int u, int v, int w) {
+                this.u = u;
+                this.v = v;
+                this.w = w;
+            }
+        }
 
 
-        static int n, m;
+        int N = 4;//顶点的个数
+        int E = 8;//边的条数
+        int[][] G = new int[N][N];//顶点之间最短距离的矩阵
 
-        Integer INF = 0x3f3f3f3f;
-        int[][] dist;
+        int[][] path = new int[N][N];
+        int[][] dist = new int[N][N];
+
+        Integer INF = Integer.MAX_VALUE;
+
 
         void init() {
-            for (int i = 1; i < N; i++) {
-                for (int j = 1; j < N; j++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
                     if (i != j) G[i][j] = INF;
+                    path[i][j] = -1;
                 }
             }
         }
@@ -35,43 +83,51 @@ public class Floyd2nd {
 
         public void testOne() {
             init();
-//            PrintUtils.printMatrix();
+            initEdge();
+            PrintUtils.printMatrix(G, 10);
+            PrintUtils.printMatrix(path, 1);
+            floyd(path, dist);
+            printHelper();
+        }
 
+        private void printHelper() {
+            // 打印floyd最短路径的结果
+            System.out.printf("floyd dist: \n");
+            PrintUtils.printMatrix(dist, 2);
+            System.out.printf("floyd path: \n");
+            PrintUtils.printMatrix(path, 2);
+            int u = 3, v = 0;
+            System.out.printf("%d--->", u);
+            printPath(path, u, v);
+            System.out.printf("%d\n", v);
         }
 
 
-        public void testTwo() {
-            init();
-            Scanner sc = new Scanner(System.in);
-            n = sc.nextInt();//顶点数量
-            m = sc.nextInt();//边的条数
-            int u, v, w;
-            while (m-- > 0) {
-                u = sc.nextInt();//from
-                v = sc.nextInt();//to
-                w = sc.nextInt();//weigh
-                G[u][v] = G[v][u] = w;
+        private void printPath(int[][] path, int u, int v) {
+            if (path[u][v] == -1) {
+                return;
             }
-            floyd();
-            System.out.println(G[1][n]);
+            int mid = path[u][v];
+            System.out.printf("%d--->", mid);
+            printPath(path, u, mid);
+            printPath(path, mid, v);
+
         }
 
-
-//    strictfp
-
-        static void floyd() {
-            for (int k = 1; k <= n; k++) {
-                for (int i = 1; i <= n; i++) {
-                    for (int j = 1; j <= n; j++) {
-                        G[i][j] = Math.min(G[i][j], G[i][k] + G[k][j]);
-                    }
-                }
+        private void initEdge() {
+            Edge[] edges = new Edge[E];
+            edges[0] = new Edge(0, 1, 5);
+            edges[1] = new Edge(0, 3, 7);
+            edges[2] = new Edge(1, 2, 4);
+            edges[3] = new Edge(1, 3, 2);
+            edges[4] = new Edge(2, 0, 3);
+            edges[5] = new Edge(2, 1, 3);
+            edges[6] = new Edge(2, 3, 2);
+            edges[7] = new Edge(3, 2, 1);
+            for (int i = 0; i < E; i++) {
+                G[edges[i].u][edges[i].v] = edges[i].w;
             }
         }
-
-
-
-
 
         /*
          * floyd最短路径。
@@ -81,41 +137,31 @@ public class Floyd2nd {
          *     path -- 路径。path[i][j]=k表示，"顶点i"到"顶点j"的最短路径会经过顶点k。
          *     dist -- 长度数组。即，dist[i][j]=sum表示，"顶点i"到"顶点j"的最短路径的长度是sum。
          */
-      /*  public void floyd(int[][] path, int[][] dist) {
-
-            // 初始化  mVexs // 顶点集合
-            //mMatrix 邻接矩阵 i =j 时为0 初始化时， 没有路径时是INF 其他位置是u->v的权值
-            for (int i = 0; i < mVexs.length; i++) {
-                for (int j = 0; j < mVexs.length; j++) {
-                    dist[i][j] = mMatrix[i][j];    // "顶点i"到"顶点j"的路径长度为"i到j的权值"。
-                    path[i][j] = j;                // "顶点i"到"顶点j"的最短路径是经过顶点j。
+        public void floyd(int[][] path, int[][] dist) {
+            // 初始化  N // 顶点集合
+            //G 邻接矩阵 i =j 时为0 初始化时， 没有路径时是INF 其他位置是u->v的权值
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dist[i][j] = G[i][j];    // "顶点i"到"顶点j"的路径长度为"i到j的权值"。
+//                    if (G[i][j] != INF && G[i][j] != 0) path[i][j] = j; // "顶点i"到"顶点j"的最短路径是经过顶点j。
                 }
             }
-
             // 计算最短路径
-            for (int k = 0; k < mVexs.length; k++) {
-                for (int i = 0; i < mVexs.length; i++) {
-                    for (int j = 0; j < mVexs.length; j++) {
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
                         // 如果经过下标为k顶点路径比原两点间路径更短，则更新dist[i][j]和path[i][j]
                         int tmp = (dist[i][k] == INF || dist[k][j] == INF) ? INF : (dist[i][k] + dist[k][j]);
                         if (dist[i][j] > tmp) {
                             // "i到j最短路径"对应的值设，为更小的一个(即经过k)
                             dist[i][j] = tmp;
                             // "i到j最短路径"对应的路径，经过k
-                            path[i][j] = path[i][k];
+                            path[i][j] = k;
                         }
                     }
                 }
             }
-
-            // 打印floyd最短路径的结果
-            System.out.printf("floyd: \n");
-            for (int i = 0; i < mVexs.length; i++) {
-                for (int j = 0; j < mVexs.length; j++)
-                    System.out.printf("%2d  ", dist[i][j]);
-                System.out.printf("\n");
-            }
-        }*/
+        }
 
 
     }
--- src/main/leetcode_manuscripts/classical_algorithm/一文掌握Floyd算法.md
+++ src/main/leetcode_manuscripts/classical_algorithm/一文掌握Floyd算法.md
@@ -32,12 +32,30 @@ typedef struct struct_graph{
     }
 ```
 
+#### 思路
+
+- 用数组`dist[i][j]`来记录`i,j`之间的最短距离。初始化`dist[i][j]`若`i=j`则`dist[i][j]`=0
+  - 若`i,j`之间有边连接则的$dist[i][j]$值为该边的权值，否则`dist[i][j]`的值为`INF`(无穷大)
+- 对所有的`k`值从`1`到`n`,修正任意两点之间的最短距离,计算`dist[i][k]`+`dist[k][j]`的值，若小于`dist[i][j]`,则`dist[i][j]`= `dist[i][k]`+`dist[k][j]`，否则`dist[i][j]`的值不变
+
+![floyd](D:\Dev\SrcCode\geek-algorithm-leetcode\src\main\leetcode_manuscripts\classical_algorithm\一文掌握Floyd算法.assets\floyd.png)
 
 
-#### 模板代码
+
+
+
+### 模板代码
+
+#### 核心代码
 
 ```java
-        /*
+		int N = 4;//顶点的个数
+        int E = 8;//边的条数
+        int[][] G = new int[N][N];//顶点之间最短距离的矩阵
+
+        int[][] path = new int[N][N];//路径的矩阵
+        int[][] dist = new int[N][N];//距离的矩阵
+		/*
          * floyd最短路径。
          * 即，统计图中各个顶点间的最短路径。
          *
@@ -46,45 +64,94 @@ typedef struct struct_graph{
          *     dist -- 长度数组。即，dist[i][j]=sum表示，"顶点i"到"顶点j"的最短路径的长度是sum。
          */
         public void floyd(int[][] path, int[][] dist) {
-
-            // 初始化  mVexs // 顶点集合
-            //mMatrix 邻接矩阵 i =j 时为0 初始化时， 没有路径时是INF 其他位置是u->v的权值
-            for (int i = 0; i < mVexs.length; i++) {
-                for (int j = 0; j < mVexs.length; j++) {
-                    dist[i][j] = mMatrix[i][j];    // "顶点i"到"顶点j"的路径长度为"i到j的权值"。
-                    path[i][j] = j;                // "顶点i"到"顶点j"的最短路径是经过顶点j。
+            // 初始化  N // 顶点集合
+            //G 邻接矩阵 i =j 时为0 初始化时， 没有路径时是INF 其他位置是u->v的权值
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dist[i][j] = G[i][j];    // "顶点i"到"顶点j"的路径长度为"i到j的权值"。
+//                    if (G[i][j] != INF && G[i][j] != 0) path[i][j] = j; // "顶点i"到"顶点j"的最短路径是经过顶点j。
                 }
             }
-
             // 计算最短路径
-            for (int k = 0; k < mVexs.length; k++) {
-                for (int i = 0; i < mVexs.length; i++) {
-                    for (int j = 0; j < mVexs.length; j++) {
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
                         // 如果经过下标为k顶点路径比原两点间路径更短，则更新dist[i][j]和path[i][j]
                         int tmp = (dist[i][k] == INF || dist[k][j] == INF) ? INF : (dist[i][k] + dist[k][j]);
                         if (dist[i][j] > tmp) {
                             // "i到j最短路径"对应的值设，为更小的一个(即经过k)
                             dist[i][j] = tmp;
                             // "i到j最短路径"对应的路径，经过k
-                            path[i][j] = path[i][k];
+                            path[i][j] = k;
                         }
                     }
                 }
             }
-
-            // 打印floyd最短路径的结果
-            System.out.printf("floyd: \n");
-            for (int i = 0; i < mVexs.length; i++) {
-                for (int j = 0; j < mVexs.length; j++)
-                    System.out.printf("%2d  ", dist[i][j]);
-                System.out.printf("\n");
-            }
         }
-
-    }
 ```
 
+#### 测试
 
+```java
+       
+		public void testOne() {
+            init();
+            initEdge();
+            PrintUtils.printMatrix(G, 10);
+            PrintUtils.printMatrix(path, 1);
+            floyd(path, dist);
+            printHelper();
+        }
+		void init() {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i != j) G[i][j] = INF;
+                    path[i][j] = -1;
+                }
+            }
+        }
+		//初始化边，生成graph，使用的是图例的demo
+        private void initEdge() {
+            Edge[] edges = new Edge[E];
+            edges[0] = new Edge(0, 1, 5);
+            edges[1] = new Edge(0, 3, 7);
+            edges[2] = new Edge(1, 2, 4);
+            edges[3] = new Edge(1, 3, 2);
+            edges[4] = new Edge(2, 0, 3);
+            edges[5] = new Edge(2, 1, 3);
+            edges[6] = new Edge(2, 3, 2);
+            edges[7] = new Edge(3, 2, 1);
+            for (int i = 0; i < E; i++) {
+                G[edges[i].u][edges[i].v] = edges[i].w;
+            }
+        }
+		//打印
+		private void printHelper() {
+            // 打印floyd最短路径的结果
+            System.out.printf("floyd dist: \n");
+            PrintUtils.printMatrix(dist, 2);
+            System.out.printf("floyd path: \n");
+            PrintUtils.printMatrix(path, 2);
+            int u = 3, v = 0;
+            System.out.printf("%d--->", u);
+            printPath(path, u, v);
+            System.out.printf("%d\n", v);
+        }
+public class PrintUtils {
+    public static void printMatrix(int[][] matrix, int d) {
+        int rows = matrix.length, cols = matrix[0].length;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                System.out.print(String.format("%" + d + "s", matrix[i][j]) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------");
+    } 
+}
+```
+
+#### 番外
 
 ```java
     int[][] path;
@@ -103,8 +170,6 @@ typedef struct struct_graph{
         findPath(k, j);//k->j
     }
 ```
-
-
 
 ### Reference
 
