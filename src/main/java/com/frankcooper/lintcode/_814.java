72d9ec4af7f36f6338890b261e15a7f72aeebad2
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-09-26 18:00:05 +0800

    [geek-algorithm-leetcode] 814

---
+++ src/main/java/com/frankcooper/lintcode/_814.java
@@ -0,0 +1,45 @@
+package com.frankcooper.lintcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+// * 同leetcode 323
+ * 814. 无向图中的最短路径
+ */
+public class _814 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+    class _1st{
+
+
+
+
+
+        class UndirectedGraphNode {
+            int label;
+            List<UndirectedGraphNode> neighbors;
+            UndirectedGraphNode(int x) {
+                label = x;
+                neighbors = new ArrayList<UndirectedGraphNode>();
+            }
+        }
+
+        public int shortestPath(List<UndirectedGraphNode> graph, UndirectedGraphNode A, UndirectedGraphNode B) {
+            // Write your code here
+
+            return 0;
+        }
+
+
+    }
+
+
+}
