f1c820cc4347aabff4344059e6fbbeb610881a34
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-10-11 16:35:26 +0800

    [geek-algorithm-leetcode] 210 weekly

--- src/main/java/com/frankcooper/bank/_978.java
+++ src/main/java/com/frankcooper/bank/_978.java
@@ -11,34 +11,42 @@ public class _978 {
         int[] A = {9, 4, 2, 10, 7, 8, 8, 1, 9};
         handler.maxTurbulenceSize(A);
 
+
     }
 
 
     public int maxTurbulenceSize(int[] A) {
-        if (A == null || A.length == 0) return 0;
-        int i = 0, j = 1;
-        int res = 0;
-        while (i < j && j < A.length) {
-            int k = i;
-            while (k < j) {
-                if (k % 2 == 0 && A[k] > A[k + 1]) {
-                    if (k - 1 >= 0 && A[k - 1] < A[k]) {
-                        k++;
-                    } else {
-                        k++;
-                    }
-                } else if (k % 2 == 1 && A[k] > A[k + 1]) {
-                    if (k - 1 >= 0 && A[k - 1] < A[k]) {
-                        k++;
-                    } else {
-                        k++;
-                    }
-                } else {
-                    i = k;
+        int n = A.length, l = 0;
+        int res = 1;
+        for (int r = 1; r < n; r++) {
+            int c = Integer.compare(A[r - 1], A[r]);
+            if (r == n - 1 || c * Integer.compare(A[r], A[r + 1]) != -1) {
+                if (c != 0) {
+                    res = Math.max(res, r - l + 1);
                 }
+                l = r;
             }
-            res = Math.max(res, j - i + 1);
-            j++;
+        }
+        return res;
+    }
+
+
+    public int maxTurbulenceSize1st(int[] A) {
+        if (A == null || A.length == 0) return 0;
+        int res = 1;
+        int up = 1, down = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) {
+                up = down + 1;
+                down = 1;
+            } else if (A[i] < A[i - 1]) {
+                down = up + 1;
+                up = 1;
+            } else {
+                up = 1;
+                down = 1;
+            }
+            res = Math.max(res, Math.max(up, down));
         }
         return res;
     }
