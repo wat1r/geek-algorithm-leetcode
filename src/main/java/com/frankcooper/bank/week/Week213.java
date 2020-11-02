fd829a8e7a4682d8ce54fd7d62eea9abc8cb9745
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-11-02 21:23:45 +0800

    [geek-algorithm-leetcode] week 213

--- src/main/java/com/frankcooper/bank/week/Week213.java
+++ src/main/java/com/frankcooper/bank/week/Week213.java
@@ -1,5 +1,7 @@
 package com.frankcooper.bank.week;
 
+import com.frankcooper.bank._1;
+
 import java.util.*;
 
 /**
@@ -117,6 +119,132 @@ public class Week213 {
             }
         }
 
+
+    }
+
+    static class _3rd_1 {
+
+        static _3rd_1 handler = new _3rd_1();
+
+        public static void main(String[] args) {
+
+            int[] heights = {4, 2, 7, 6, 9, 14, 12};
+            int bricks = 5;
+            int ladders = 1;
+
+            heights = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            bricks = 1;
+            ladders = 1;
+            handler.furthestBuilding(heights, bricks, ladders);
+
+        }
+
+
+        public int furthestBuilding(int[] heights, int bricks, int ladders) {
+            if (heights == null || heights.length == 0) return 0;
+            if (bricks == 0 && ladders == 0) return heights[1] - heights[0] <= 0 ? 1 : 0;
+            Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+            int idx = 1;
+            int sum = 0;
+            while (idx < heights.length) {
+                int delta = heights[idx] - heights[idx - 1];
+                if (delta > 0) {
+                    sum += delta;
+                    pq.offer(delta);
+                }
+                if (sum > bricks) {
+                    sum -= pq.poll();
+                    ladders--;
+                }
+                if (ladders < 0) return idx - 1;
+                if (ladders == 0 && sum > bricks) break; //写了个= 第53个用例没过
+                idx++;
+            }
+            return idx - 1;
+        }
+    }
+
+    /**
+     * 先选砖头 再选梯子
+     */
+    static class _3rd_2 {
+        static _3rd_2 handler = new _3rd_2();
+
+        public static void main(String[] args) {
+
+        }
+
+
+        public int furthestBuilding(int[] heights, int bricks, int ladders) {
+            int n = heights.length, sum = 0;
+            int i = 1;
+            Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1); //大顶堆
+            while (i < n) {
+                int delta = heights[i] - heights[i - 1];
+                if (delta > 0) {
+                    sum += delta;
+                    pq.offer(delta);
+                    if (sum > bricks) {
+                        sum -= pq.poll();
+                        ladders--;
+                    }
+                    if (ladders < 0) return i - 1;
+                }
+                i++;
+            }
+            return i - 1;
+        }
+    }
+
+
+    /**
+     * 小顶堆
+     */
+    static class _3rd_3 {
+        static _3rd_3 handler = new _3rd_3();
+
+        public static void main(String[] args) {
+
+        }
+
+        public int furthestBuilding(int[] heights, int bricks, int ladders) {
+            int n = heights.length, sum = 0;
+            int i = 1;
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            while (i < n) {
+                int delta = heights[i] - heights[i - 1];
+                if (delta > 0) {
+                    pq.offer(delta);
+                    if (pq.size() > ladders) {
+                        sum += pq.poll();
+                    }
+                    if (sum > bricks) return i - 1;
+                }
+                i++;
+            }
+            return n - 1;
+        }
+    }
+
+
+    /**
+     * https://leetcode-cn.com/problems/kth-smallest-instructions/solution/javazu-he-shu-qiu-jie-si-lu-fen-xiang-by-hello-ang/
+     */
+    static class _4th_1 {
+
+        static _4th_1 handler = new _4th_1();
+
+
+        public static void main(String[] args) {
+
+//            Math.
+
+        }
+
+        public String kthSmallestPath(int[] destination, int k) {
+
+            return null;
+        }
 
     }
 
