0ccf86890dfe42c8d8083dad8784f1c912d59cf5
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2020-11-30 20:43:49 +0800

    [geek-algorithm-leetcode] 767

--- src/main/java/com/frankcooper/bank/_767.java
+++ src/main/java/com/frankcooper/bank/_767.java
@@ -2,6 +2,8 @@ package com.frankcooper.bank;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class _767 {
 
@@ -10,31 +12,71 @@ public class _767 {
     public static void main(String[] args) {
 //        handler.reorganizeString("aaab");
 //        handler.reorganizeString("aab");
-        handler.reorganizeString("vvvlo");
+//        handler.reorganizeString("vvvloo");
     }
 
-    @Deprecated
-    public String reorganizeString(String S) {
-        int n = S.length();
-        Integer[] arr = new Integer[26];
-        Arrays.fill(arr, 0);
-        for (char c : S.toCharArray()) {
-            arr[c - 97]++;
-        }
-        Arrays.sort(arr, Collections.reverseOrder());
-        if (arr[0] > (n + 1) / 2) return "";
-        StringBuilder sb = new StringBuilder();
-        int cnt = 0;
-        while (cnt != n) {
+    class _1st {
+        public String reorganizeString(String S) {
+            int n = S.length();
+            int[] arr = new int[26];
+            PriorityQueue<Pair> pq = new PriorityQueue<>(((o1, o2) -> o2.cnt - o1.cnt));
+            for (char c : S.toCharArray()) {
+                arr[c - 'a']++;
+            }
             for (int i = 0; i < 26; i++) {
-                if (arr[i] != 0) {
-                    sb.append((char) (i + 97));
-                    cnt++;
-                    arr[i]--;
+                if (arr[i] > (n + 1) / 2) return "";
+                if (arr[i] > 0) {
+                    pq.offer(new Pair(arr[i], (char) (i + 'a')));
                 }
             }
+            StringBuilder sb = new StringBuilder();
+            Pair prev = new Pair(0, ' ');
+            while (!pq.isEmpty()) {
+                Pair curr = pq.poll();
+                sb.append(curr.c);
+                if (prev.cnt > 0) {
+                    pq.offer(prev);
+                }
+                prev = new Pair(curr.cnt - 1, curr.c);
+            }
+            return sb.toString();
         }
-        return sb.toString();
+
+
+        class Pair {
+            private int cnt;
+            private char c;
+
+            public Pair(int cnt, char c) {
+                this.cnt = cnt;
+                this.c = c;
+            }
+        }
+
+    }
+
+    class _2nd {
+
+        public String reorganizeString(String S) {
+            int n = S.length();
+            int[] arr = new int[26];
+            for (char c : S.toCharArray()) arr[c - 'a']++;
+            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] > (n + 1) / 2) return "";
+                if (arr[i] > 0) pq.offer(new int[]{i + 'a', arr[i]});
+            }
+            int[] prev = {-1, 0};
+            StringBuilder sb = new StringBuilder();
+            while (!pq.isEmpty()) {
+                int[] curr = pq.poll();
+                sb.append((char) curr[0]);
+                if (prev[1] > 0) pq.offer(prev);
+                prev = new int[]{curr[0], curr[1] - 1};
+            }
+            return sb.toString();
+        }
+
     }
 
 }
