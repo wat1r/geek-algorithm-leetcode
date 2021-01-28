be33d307b8e594392b30031c2dd0c01f2766aeb1
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-01-28 21:22:34 +0800

    [geek-algorithm-leetcode] 1319

--- src/main/java/com/frankcooper/bank/_121.java
+++ src/main/java/com/frankcooper/bank/_121.java
@@ -6,6 +6,25 @@ public class _121 {
 
     }
 
+
+    class _1st {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length == 0) return 0;
+            int n = prices.length;
+            int[][] dp = new int[n][2];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+            for (int i = 1; i < n; i++) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            }
+            return dp[n - 1][0];
+        }
+
+    }
+
+
+
     /*
        ##### 方法1：基础版DP
 
