8887decc22dce5e70b10e19ec1c0839a6fcff0e6
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-02-03 09:01:29 +0800

    [geek-algorithm-leetcode]  480

---
+++ src/main/java/com/frankcooper/bank/_424.java
@@ -0,0 +1,35 @@
+package com.frankcooper.bank;
+
+public class _424 {
+
+
+    static _424 handler = new _424();
+
+    public static void main(String[] args) {
+        String s = "ABAB";
+        int k = 2;
+        s = "AABABBA";
+        k = 1;
+        handler.characterReplacement(s, k);
+    }
+
+
+    public int characterReplacement(String s, int k) {
+        int[] arr = new int[26];
+        char[] chas = s.toCharArray();
+        int l = 0, r = 0, ans = 0, maxCnt = 0;
+        while (r < chas.length) {
+            arr[chas[r] - 'A']++;
+            maxCnt = Math.max(maxCnt, arr[chas[r] - 'A']);
+            r++;
+            if ((r - l) > (maxCnt + k)) {
+                arr[chas[l] - 'A']--;
+                l++;
+            }
+            ans = r - l;
+        }
+        return ans;
+    }
+
+
+}
