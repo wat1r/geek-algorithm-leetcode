bc60e7d9e84ddd72488a0ab7aedd4e93974d3128
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-02-20 09:46:41 +0800

    [geek-algorithm-leetcode] 697

--- src/main/java/com/frankcooper/bank/_151.java
+++ src/main/java/com/frankcooper/bank/_151.java
@@ -3,6 +3,12 @@ package com.frankcooper.bank;
 public class _151 {
 
 
+    public static void main(String[] args) {
+        _2nd handler = new _2nd();
+        String s = "the sky is blue";
+        handler.reverseWords(s);
+    }
+
     static class _1st {
         public String reverseWords(String s) {
             if (s == null || s.length() == 0) return null;
@@ -35,8 +41,44 @@ public class _151 {
         /**
          * https://leetcode.com/problems/reverse-words-in-a-string/discuss/47720/Clean-Java-two-pointers-solution-(no-trim(-)-no-split(-)-no-StringBuilder
          */
+        public String reverseWords(String s) {
+            char[] chas = s.toCharArray();
+            int n = chas.length;
+            reverse(chas, 0, chas.length - 1);
+            reverseWords(chas, n);
+            return cleanSpaces(chas, n);
+        }
 
-        
+
+        public String cleanSpaces(char[] chas, int n) {
+            int l = 0, r = 0;
+            while (r < n) {
+                while (r < n && chas[r] == ' ') r++;
+                while (r < n && chas[r] != ' ') chas[l++] = chas[r++];
+                while (r < n && chas[r] == ' ') r++;
+                if (r < n) chas[l++] = ' ';
+            }
+            return new String(chas).substring(0, l);
+        }
+
+        public void reverseWords(char[] chas, int n) {
+            int l = 0, r = 0;
+            while (l < chas.length) {
+                while (l < r || l < n && chas[l] == ' ') l++;
+                while (r < l || r < n && chas[r] != ' ') r++;
+                reverse(chas, l, r - 1);
+            }
+        }
+
+
+        public void reverse(char[] chas, int l, int r) {
+            while (l < r) {
+                char t = chas[l];
+                chas[l++] = chas[r];
+                chas[r--] = t;
+            }
+        }
+
     }
 
 }
