c180203a5bb4d44b354b5e2cd9eda9f8ba77dd9f
wat1r <cnwangzhou@hotmail.com>
2021-03-12 09:41:19 +0800

    设计FaceBook Messenger.md

---
+++ src/main/java/com/frankcooper/bank/_153.java
@@ -0,0 +1,29 @@
+package com.frankcooper.bank;
+
+public class _153 {
+
+    static class _1st {
+
+
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+            int[] nums = new int[]{3, 4, 5, 1, 2};
+            handler.findMin(nums);
+        }
+
+        public int findMin(int[] nums) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int m = l + (r - l) / 2;
+                if (nums[m] == nums[r]) return nums[m];
+                else if (nums[m] > nums[r]) {
+                    l = m + 1;
+                } else if (nums[m] < nums[r]) {
+                    r = m;
+                }
+            }
+            return nums[l];
+        }
+
+    }
+}
