451f7ca2cbcc08a038f6c36f176df7853f2efcff
wat1r <cnwangzhou@hotmail.com>
2021-03-19 09:01:12 +0800

    698

--- src/main/java/com/frankcooper/bank/_501_1000/_698.java
+++ src/main/java/com/frankcooper/bank/_501_1000/_698.java
@@ -15,36 +15,57 @@ public class _698 {
             int[] nums = {4, 3, 2, 3, 5, 2, 1};
             int k = 4;
 //            Assert.assertTrue(handler.canPartitionKSubsets(nums,k));
-            nums = new int[]{2,2,2,2,3,4,5};
+            nums = new int[]{2, 2, 2, 2, 3, 4, 5};
             k = 4;
-            Assert.assertFalse(handler.canPartitionKSubsets(nums,k));
+            Assert.assertFalse(handler.canPartitionKSubsets(nums, k));
 
         }
 
 
         /**
-         * WA
+         *
          * @param nums
-         * @param k
+         * @param K
          * @return
          */
-        public boolean canPartitionKSubsets(int[] nums, int k) {
+
+
+        public boolean canPartitionKSubsets(int[] nums, int K) {
             int N = nums.length;
             int sum = 0;
             for (int i : nums) sum += i;
-            if (sum % k != 0) return false;
-            int T = sum / k;
-            boolean[][] f = new boolean[N][T + 1];
-            for (int i = 0; i < N; i++) f[i][0] = true;
-            for (int j = 0; j <= T; j++) if (j == nums[0]) f[0][j] = true;
-            for (int i = 1; i < N; i++) {
-                for (int j = 1; j <= T; j++) {
-                    f[i][j] = f[i - 1][j];
-                    if (j >= nums[i]) f[i][j] = f[i - 1][j] || f[i - 1][j - nums[i]];
+            if (sum % K != 0) return false;//如果不能被K整除，返回false
+            int T = sum / K;//每个子集的目标和 target
+            boolean[] vis = new boolean[N];
+            return backtracing(nums, vis, K, T, 0, 0);
+        }
+
+        /**
+         * @param nums   给定的子集数组
+         * @param vis    记录该子集数组的元素是否访问过
+         * @param k      当前处理到还剩下的子集的个数，总的是K
+         * @param target 每个子集的目标和 也就是T
+         * @param curr   当前这个处理到的这个潜在的子集的和
+         * @param idx    当前处理到的子集数组的下标
+         * @return
+         */
+        public boolean backtracing(int[] nums, boolean[] vis, int k, int target, int curr, int idx) {
+            //还剩下一个子集时候，已经结束了
+            if (k == 1) return true;
+            //或者下面的剪枝可以写这里
+//            if(curr>target) return false;
+            //当前子集的和到target的时候，需要进行下一个子集的探测，已经找到了当前子集，所以k-1， 重新开始找子集，curr 和 idx 重新初始化
+            if (curr == target) return backtracing(nums, vis, k - 1, target, 0, 0);
+            for (int i = idx; i < nums.length; i++) {
+                //这里的剪枝 && curr + nums[i] <= target 起到很大加速作用，如果当前的值，加上我们要的这个i进入下一层，已经大于target这个值时，说明溢出了，没必要继续了
+                if (!vis[i] && curr + nums[i] <= target) {
+                    vis[i] = true;
+                    if (backtracing(nums, vis, k, target, curr + nums[i], i + 1)) return true;
+                    vis[i] = false;
                 }
             }
-            PrintUtils.printMatrix(f);
-            return f[N - 1][T];
+            return false;
+
         }
 
     }
