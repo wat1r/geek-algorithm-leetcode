eb75c4657e6507058e593858eb0e8108bbfd6f0f
wat1r <cnwangzhou@hotmail.com>
2021-03-29 09:43:25 +0800

    Math

--- src/main/java/com/frankcooper/bank/week/CommonFunction.java
+++ src/main/java/com/frankcooper/bank/week/CommonFunction.java
@@ -1,10 +1,14 @@
 package com.frankcooper.bank.week;
 
+import org.junit.Assert;
+
 public class CommonFunction {
 
 
     /**
+     * Greatest Common Divisor(GCD)
      * 最大公约数
+     * 最大公因数，也称最大公约数、最大公因子，指两个或多个整数共有约数中最大的一个。a，b的最大公约数记为（a，b）
      *
      * @param a
      * @param b
@@ -31,7 +35,7 @@ public class CommonFunction {
     /**
      * 计算 a^b  模上MOD
      */
-    public long pow(long a, long b, long mod) {
+    public long pow1(long a, long b, long mod) {
         a %= mod;
         if (b < 0) {
             a = inverse(a, mod);
@@ -45,5 +49,25 @@ public class CommonFunction {
         }
         return ans % mod;
     }
+
+
+    /*
+    计算 a^b
+     */
+    public long pow(long a, long b) {
+        long ans = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) ans *= a;
+            a *= a;
+            b /= 2;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        CommonFunction handler = new CommonFunction();
+        Assert.assertEquals(handler.pow(9, 8), (int) Math.pow(9, 8));
+    }
+
 
 }
