46c9ed0bd0e3ea5bacec558e26163089b0e7ef0c
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-03-31 20:48:32 +0800

    [geek-algorithm-leetcode] Math

--- src/main/java/com/frankcooper/bank/_1_100/_90.java
+++ src/main/java/com/frankcooper/bank/_1_100/_90.java
@@ -1,8 +1,10 @@
 package com.frankcooper.bank._1_100;
 
+import org.junit.Assert;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class _90 {
 
@@ -87,4 +89,77 @@ public class _90 {
             }
         }
     }
+
+
+    static class _4th {
+
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+            int[] nums = new int[]{4, 4, 4, 1, 4};
+//            [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+            List<List<Integer>> actual = new ArrayList<List<Integer>>() {{
+                add(new ArrayList<>());
+                add(Arrays.asList(1));
+                add(Arrays.asList(1, 4));
+                add(Arrays.asList(1, 4, 4));
+                add(Arrays.asList(1, 4, 4, 4));
+                add(Arrays.asList(1, 4, 4, 4, 4));
+                add(Arrays.asList(4));
+                add(Arrays.asList(4, 4));
+                add(Arrays.asList(4, 4, 4));
+                add(Arrays.asList(4, 4, 4, 4));
+            }};
+            Assert.assertEquals(handler.subsetsWithDup(nums), actual);
+        }
+
+        /**
+         * [4, 4, 4, 1, 4]
+         */
+        Set<String> set = new HashSet<>();
+
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> res = new ArrayList<>();
+            res.add(new ArrayList<>());
+            for (int i = 0; i < nums.length; i++) {
+                List<Integer> sub = new ArrayList<>();
+                dfs(res, sub, i, nums);
+            }
+            return res;
+        }
+
+        private void dfs(List<List<Integer>> res, List<Integer> sub, int start, int[] nums) {
+            if (start >= nums.length) {
+                return;
+            }
+            sub.add(nums[start]);
+            StringBuilder sb = new StringBuilder();
+            for (Integer val : sub) {
+                sb.append(val).append("-");
+            }
+            System.out.printf("%s\n", sb.toString());
+            String str = sb.toString();
+            if (!set.contains(str)) {
+                res.add(new ArrayList<>(sub));
+                set.add(str);
+            }
+            for (int i = start + 1; i < nums.length; i++) {
+                dfs(res, sub, i, nums);
+            }
+            sub.remove(sub.size() - 1);
+        }
+    }
+
+
+    static class _5th {
+
+        public static void main(String[] args) {
+
+            System.out.printf("%s\n", "ddd");
+
+
+//
+        }
+    }
+
 }
