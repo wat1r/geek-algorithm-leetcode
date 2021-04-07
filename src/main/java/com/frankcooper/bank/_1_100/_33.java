0089c356b5f288cf4bb0645d5440b0d47097ebb7
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-04-07 20:59:23 +0800

    [geek-algorithm-leetcode] LCP

--- src/main/java/com/frankcooper/bank/_1_100/_33.java
+++ src/main/java/com/frankcooper/bank/_1_100/_33.java
@@ -1,5 +1,7 @@
 package com.frankcooper.bank._1_100;
 
+import org.junit.Assert;
+
 public class _33 {
 
     static class _1st {
@@ -34,6 +36,38 @@ public class _33 {
                     } else {
                         r = m;
                     }
+                }
+            }
+            return nums[l] == target ? l : -1;
+        }
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+            int[] nums = {1, 3};
+            int target = 3;
+            Assert.assertEquals(handler.search(nums, target), 1);
+            nums = new int[]{4, 5, 6, 7, 8, 1, 2, 3};
+            target = 8;
+            Assert.assertEquals(handler.search(nums, target), 4);
+            nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+            target = 0;
+            Assert.assertEquals(handler.search(nums, target), 4);
+        }
+
+
+        public int search(int[] nums, int target) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int m = l + (r - l) / 2;
+                if (nums[m] == target) return m;
+                else if (nums[m] >= nums[l]) {
+                    if (target >= nums[l] && target <= nums[m]) r = m;
+                    else l = m + 1;
+                } else if (nums[m] < nums[l]) {
+                    if (target >= nums[m] && target <= nums[r]) l = m + 1;
+                    else r = m;
                 }
             }
             return nums[l] == target ? l : -1;
--- src/main/java/com/frankcooper/bank/_1_100/_81.java
+++ src/main/java/com/frankcooper/bank/_1_100/_81.java
@@ -27,4 +27,23 @@ public class _81 {
             return false;
         }
     }
+
+
+    static class _2nd {
+        public boolean search(int[] nums, int target) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int m = l + (r - l) / 2;
+                if (nums[m] == target) return true;
+                if (nums[m] > nums[l]) {
+                    if (target >= nums[l] && target <= nums[m]) r = m - 1;
+                    else l = m + 1;
+                } else if (nums[m] < nums[l]) {
+                    if (target >= nums[m] && target <= nums[r]) l = m + 1;
+                    else r = m - 1;
+                } else if (nums[m] == nums[l]) l++;
+            }
+            return nums[l] == target;//多加一层判断
+        }
+    }
 }
