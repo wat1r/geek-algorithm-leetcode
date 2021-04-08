a4172ace08e90d784fef80387457b4c58b3c1359
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-04-08 09:45:57 +0800

    [geek-algorithm-leetcode] Daily

--- src/main/java/com/frankcooper/bank/_101_200/_153.java
+++ src/main/java/com/frankcooper/bank/_101_200/_153.java
@@ -26,4 +26,16 @@ public class _153 {
         }
 
     }
+
+    static class _2nd {
+        public int findMin(int[] nums) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] > nums[r]) l = mid + 1;
+                else r = mid;
+            }
+            return nums[r];
+        }
+    }
 }
--- src/main/java/com/frankcooper/bank/_101_200/_154.java
+++ src/main/java/com/frankcooper/bank/_101_200/_154.java
@@ -22,4 +22,27 @@ public class _154 {
             return nums[l];
         }
     }
+
+
+    static class _2nd {
+
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+            int[] nums = new int[]{2, 2, 2, 0, 1};
+            nums = new int[]{3, 3, 1, 3};
+            handler.findMin(nums);
+
+        }
+
+        public int findMin(int[] nums) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] > nums[r]) l = mid + 1;
+                else if (nums[mid] < nums[r]) r = mid;
+                else r = r - 1;
+            }
+            return nums[r];
+        }
+    }
 }
