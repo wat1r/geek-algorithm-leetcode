178e55e78bf3a571b9a1b2551bff9d35e37355c0
wat1r <cnwangzhou@hotmail.com>
2021-04-20 22:28:27 +0800

    Daily

---
+++ src/main/java/com/frankcooper/bank/_1_100/_28.java
@@ -0,0 +1,50 @@
+package com.frankcooper.bank._1_100;
+
+import java.util.*;
+
+import org.junit.Assert;
+
+public class _28 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+        }
+
+
+        public int strStr(String haystack, String needle) {
+            if (haystack == null || needle == null || haystack.length() < needle.length()) {
+                return -1;
+            }
+            if (needle.length() == 0) {
+                return 0;
+            }
+            for (int i = 0; i < haystack.length(); i++) {
+                if (i + needle.length() > haystack.length()) break;
+                if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
+            }
+            return -1;
+        }
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
