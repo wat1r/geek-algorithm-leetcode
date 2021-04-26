08bdac3f541ac5732d4974e6f9d121252e0230c8
wat1r <cnwangzhou@hotmail.com>
2021-04-27 06:37:48 +0800

    Daily

---
+++ src/main/java/com/frankcooper/bank/_1001_2000/_1011.java
@@ -0,0 +1,68 @@
+package com.frankcooper.bank._1001_2000;
+
+
+import java.util.*;
+
+import org.junit.Assert;
+
+public class _1011 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+            int[] weights = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int D = 5;
+            Assert.assertEquals(15, handler.shipWithinDays(weights, D));
+        }
+
+
+        public int shipWithinDays(int[] weights, int D) {
+            int lo = 1, hi = 0;//二分上界
+            for (int w : weights) hi += w;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;//左移左动，向下取整
+                //判断这个数是否满足要求
+                int t = 0; //目标的可能天数
+                int cur = 0;
+                for (int w : weights) {
+                    if (cur > mid) {
+                        cur = w;
+                        t++;
+                        continue;
+                    }
+                    if (t > D) {
+                        break;
+                    }
+                    cur += w;
+                }
+                if (t >= D) {
+                    hi = mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            return lo;
+        }
+
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
