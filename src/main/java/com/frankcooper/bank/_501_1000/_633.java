053f11e53317a48ab9042293a8825cb4014f28b2
wat1r <cnwangzhou@hotmail.com>
2021-04-28 08:49:24 +0800

    Daily

--- src/main/java/com/frankcooper/bank/_501_1000/_633.java
+++ src/main/java/com/frankcooper/bank/_501_1000/_633.java
@@ -11,41 +11,58 @@ public class _633 {
             _1st handler = new _1st();
 //            Assert.assertTrue(handler.judgeSquareSum(5));
 //            Assert.assertTrue(handler.judgeSquareSum(4));
-            Assert.assertTrue(handler.judgeSquareSum(2));
+//            Assert.assertTrue(handler.judgeSquareSum(2));
+            Assert.assertTrue(handler.judgeSquareSum(100));
 
             //2147483645  101/124
         }
 
 
         public boolean judgeSquareSum(int c) {
-            int lo = 0, hi = (int) Math.sqrt(c);
-            while (hi > 0) {
-                while (lo <= hi) {
-                    int t = lo * lo + hi * hi;
-                    if (t == c) return true;
-                    else if (t > c) break;
-                    else if (t < c) lo++;
-                }
-                lo = 0;
-                hi--;
+            long lo = 0, hi = (long) Math.sqrt(c);
+            while (lo <= hi) {
+                long t = lo * lo + hi * hi;//可以用 c-lo*lo == hi*hi 防止溢出
+                if (t == c) return true;
+                else if (t > c) hi--;
+                else if (t < c) lo++;
             }
-            return c == 0;
+            return false;
         }
 
-
-//        public boolean check(int target, int lo, int hi) {//判断 lo 和 hi之间能不能有两个数平方和 = target 固定了 hi
-//            while (lo < hi) {
-//
-//            }
-//
-//        }
 
     }
 
     static class _2nd {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
+//            Assert.assertTrue(handler.judgeSquareSum(100));
+            Assert.assertTrue(handler.judgeSquareSum(10000));
         }
+
+        public boolean judgeSquareSum(int c) {
+            // n % 2^n = n & 2^n-1
+            if ((c & 3) == 3)
+                return false;
+            for (int i = 2; i * i <= c; i++) {
+                System.out.printf("%d\n", i);
+                // 求c的质因子
+                if (c % i != 0)
+                    continue;
+                // 质因子的幂次
+                int count = 0;
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                // 引理2
+                if ((i & 3) == 3 && (count & 1) == 1)
+                    return false;
+            }
+            // 此c非彼c
+            return (c & 3) != 3;
+        }
+
+
     }
 
 
