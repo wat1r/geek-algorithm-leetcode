4c037cdc27cb8dcf4e0ce87bfd514a5d2a9dbcf7
wat1r <cnwangzhou@hotmail.com>
2021-05-20 22:53:35 +0800

    craking

---
+++ src/main/java/com/frankcooper/interview/_01_05.java
@@ -0,0 +1,51 @@
+package com.frankcooper.interview;
+
+import java.util.*;
+
+import org.junit.Assert;
+
+public class _01_05 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+        }
+
+        public boolean oneEditAway(String first, String second) {
+
+            int m = first.length(), n = second.length();
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 0; i <= m; i++) dp[i][0] = i;
+            for (int j = 0; j <= n; j++) dp[0][j] = j;
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (first.charAt(i - 1) == second.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                    else dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+            return dp[m][n] <= 1;
+        }
+
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
