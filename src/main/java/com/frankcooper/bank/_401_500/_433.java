baf7344c1bc2165e1d719bd4b6e15a43781656d1
wat1r <cnwangzhou@hotmail.com>
2021-07-02 09:56:44 +0800

    bfs

---
+++ src/main/java/com/frankcooper/bank/_401_500/_433.java
@@ -0,0 +1,54 @@
+package com.frankcooper.bank._401_500;
+
+import java.util.*;
+
+import org.junit.Assert;
+
+public class _433 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+        }
+
+
+        public int minMutation(String start, String end, String[] bank) {
+
+            return 0;
+        }
+
+        private List<String> transform(String src) {
+            List<String> res = new ArrayList<>();
+            char[] gen = {'A', 'C', 'G', 'T'};
+            for (int i = 0; i < src.length(); i++) {
+                char c = src.charAt(i);
+                for (int j = 0; j < gen.length; j++) {
+                    if (gen[j] == c) continue;
+//
+                }
+            }
+            return res;
+        }
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
