f05e2a385bb15f6d099954a263702f0ffc2de9c5
wat1r <cnwangzhou@hotmail.com>
2021-07-05 22:05:07 +0800

    daily

--- src/main/java/com/frankcooper/bank/_101_200/_198.java
+++ src/main/java/com/frankcooper/bank/_101_200/_198.java
@@ -101,4 +101,16 @@ public class _198 {
         }
 
     }
+
+    static class _6th {
+        public int rob(int[] nums) {
+            int n = nums.length;
+            int[][] f = new int[n + 1][2];//[0]表示偷了，[]
+            for (int i = 1; i <= n; i++) {
+                f[i][0] = Math.max(f[i - 1][0], f[i - 1][1]);
+                f[i][1] = nums[i - 1] + f[i - 1][0];
+            }
+            return Math.max(f[n][1], f[n][0]);
+        }
+    }
 }
