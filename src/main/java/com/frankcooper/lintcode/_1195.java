25c070e4312a5f0ada960d74c91b5ac632a65e61
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-07-08 08:49:39 +0800

    binary tree

--- src/main/java/com/frankcooper/lintcode/_1195.java
+++ src/main/java/com/frankcooper/lintcode/_1195.java
@@ -39,6 +39,26 @@ public class _1195 {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
         }
+
+
+        public List<Integer> largestValues(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+            while (!q.isEmpty()) {
+                int size = q.size();
+                int maxv = 0;
+                for (int i = 0; i < size; i++) {
+                    TreeNode cur = q.poll();
+                    maxv = Math.max(maxv, cur.val);
+                    if (cur.left != null) q.offer(cur.left);
+                    if (cur.right != null) q.offer(cur.right);
+                }
+                res.add(maxv);
+            }
+            return res;
+
+        }
     }
 
 
