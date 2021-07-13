b7b0ce0ff98071201e1e65393009935e8abc9ff7
cnwangzhou@hotmail.com <wangzhou@shengqugames.com>
2021-07-13 10:04:47 +0800

    hbase

---
+++ src/main/java/com/frankcooper/struct/ParentTreeNode.java
@@ -0,0 +1,11 @@
+package com.frankcooper.struct;
+
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2021/7/13 8:57
+ * @description:
+ */
+public class ParentTreeNode {
+    public int val;
+    public ParentTreeNode parent, left, right;
+}
