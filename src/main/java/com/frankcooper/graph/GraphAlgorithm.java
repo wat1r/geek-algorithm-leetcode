d2cf77729acc092500ae1c742941e8c75116e01d
wat1r <cnwangzhou@hotmail.com>
2021-08-08 22:35:23 +0800

    graph

--- src/main/java/com/frankcooper/graph/GraphAlgorithm.java
+++ src/main/java/com/frankcooper/graph/GraphAlgorithm.java
@@ -152,4 +152,74 @@ public class GraphAlgorithm {
              */
         }
     }
+
+    //图的传递闭包
+    static class _3rd {
+        static class GraphClosure {
+            final static int V = 4;
+
+            void transitiveClosure(int graph[][]) {
+                int[][] reach = new int[V][V];
+                int i, j, k;
+                for (i = 0; i < V; i++) {
+                    for (j = 0; j < V; j++) {
+                        reach[i][j] = graph[i][j];
+                    }
+                }
+                for (k = 0; k < V; k++) {
+                    for (i = 0; i < V; i++) {
+                        for (j = 0; j < V; j++) {
+                            reach[i][j] = (reach[i][j] != 0 || (reach[i][k] != 0 && reach[k][j] != 0)) ? 1 : 0;
+                        }
+                    }
+                }
+                printSolution(reach);
+            }
+
+            void printSolution(int reach[][]) {
+                System.out.println("Following matrix is transitive closure of the given graph");
+                for (int i = 0; i < V; i++) {
+                    for (int j = 0; j < V; j++) {
+                        if (i == j)
+                            System.out.print("1 ");
+                        else
+                            System.out.print(reach[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+
+            public static void main(String[] args) {
+                /* Let us create the following weighted graph
+                   10
+                (0)------->(3)
+                |         /|\
+              5 |          |
+                |          | 1
+                \|/        |
+                (1)------->(2)
+                   3           */
+
+                /* Let us create the following weighted graph
+
+                      10
+                 (0)------->(3)
+                  |         /|\
+                5 |          |
+                  |          | 1
+                 \|/         |
+                 (1)------->(2)
+                    3           */
+                int graph[][] = new int[][]{{1, 1, 0, 1},
+                        {0, 1, 1, 0},
+                        {0, 0, 1, 1},
+                        {0, 0, 0, 1}
+                };
+
+                // Print the solution
+                GraphClosure g = new GraphClosure();
+                g.transitiveClosure(graph);
+            }
+        }
+    }
 }
