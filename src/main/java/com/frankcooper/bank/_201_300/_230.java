bf161f2b0f38f978121807b2521c724288e402b3
wat1r <cnwangzhou@hotmail.com>
2021-08-27 22:58:07 +0800

    daily

--- src/main/java/com/frankcooper/bank/_201_300/_230.java
+++ src/main/java/com/frankcooper/bank/_201_300/_230.java
@@ -34,6 +34,27 @@ public class _230 {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
         }
+
+        //迭代，一直压到left没有为止
+        public int kthSmallest(TreeNode root, int k) {
+
+            Stack<TreeNode> stk = new Stack<>();
+            while (root != null) {
+                stk.add(root);
+                root = root.left;
+            }
+            while (k != 0) {
+                TreeNode cur = stk.pop();
+                if (--k == 0) return cur.val;
+                TreeNode p = cur.right;
+                while (p != null) {
+                    stk.add(p);
+                    p = p.left;
+                }
+            }
+            return -1;
+        }
+
     }
 
 
@@ -41,11 +62,45 @@ public class _230 {
         public static void main(String[] args) {
             _3rd handler = new _3rd();
         }
+
+        int res = -1;
+        int k;
+
+        public int kthSmallest(TreeNode root, int k) {
+            this.k = k;
+            dfs(root);
+            return res;
+        }
+
+        private void dfs(TreeNode root) {
+            if (root.left != null) dfs(root.left);
+            if (--k == 0) {
+                res = root.val;
+                return;
+            }
+            if (root.right != null) dfs(root.right);
+        }
     }
 
     static class _4th {
         public static void main(String[] args) {
             _4th handler = new _4th();
+        }
+
+        public int kthSmallest(TreeNode root, int k) {
+            Stack<TreeNode> stk = new Stack<>();
+            int cnt = 0;
+            while (!stk.isEmpty() || root != null) {
+                if (root != null) {
+                    stk.push(root);
+                    root = root.left;
+                } else {
+                    TreeNode cur = stk.pop();
+                    if (++cnt == k) return cur.val;
+                    root = cur.right;
+                }
+            }
+            return -1;
         }
     }
 }
