c33d46d92f31d6298384b715f8343b267757219b
wat1r <cnwangzhou@hotmail.com>
2021-08-27 08:24:34 +0800

    daily

--- src/main/java/com/frankcooper/bank/_201_300/_283.java
+++ src/main/java/com/frankcooper/bank/_201_300/_283.java
@@ -39,4 +39,28 @@ public class _283 {
             nums[i] = 0;
         }
     }
+
+    static class _1st {
+
+        public static void main(String[] args) {
+            _1st hander = new _1st();
+            int[] nums = {0, 1, 0, 3, 12};
+            hander.moveZeroes(nums);
+        }
+
+        public void moveZeroes(int[] nums) {
+
+            for (int i = 0, j = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    swap(nums, i, j++);
+                }
+            }
+        }
+
+        public void swap(int[] arr, int m, int n) {
+            int temp = arr[m];
+            arr[m] = arr[n];
+            arr[n] = temp;
+        }
+    }
 }
