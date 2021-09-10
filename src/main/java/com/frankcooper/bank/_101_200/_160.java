b3ca34872e85f36bac7b99d8e212f17fafd4af4f
wat1r <cnwangzhou@hotmail.com>
2021-09-10 16:58:22 +0800

    重温经典

--- src/main/java/com/frankcooper/bank/_101_200/_160.java
+++ src/main/java/com/frankcooper/bank/_101_200/_160.java
@@ -29,6 +29,15 @@ public class _160 {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
         }
+
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode l1 = headA, l2 = headB;
+            while (l1 != l2) {
+                l1 = (l1 == null) ? headB : l1.next;
+                l2 = (l2 == null) ? headA : l2.next;
+            }
+            return l1;
+        }
     }
 
 
