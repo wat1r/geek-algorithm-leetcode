619721cc003e8fb0cff865a5cd127aed5ab3e634
wat1r <cnwangzhou@hotmail.com>
2021-09-10 17:45:00 +0800

    daily

--- src/main/java/com/frankcooper/bank/_201_300/_206.java
+++ src/main/java/com/frankcooper/bank/_201_300/_206.java
@@ -2,6 +2,7 @@ package com.frankcooper.bank._201_300;
 
 import java.util.*;
 
+import com.frankcooper.io.ListNodeIOUtils;
 import com.frankcooper.struct.ListNode;
 import com.frankcooper.utils.ListNodeUtils;
 import org.junit.Assert;
@@ -35,11 +36,22 @@ public class _206 {
     }
 
     static class _2nd {
+
+
         public static void main(String[] args) {
             _2nd handler = new _2nd();
         }
 
-
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null, cur = head;
+            while (cur != null) {
+                ListNode nxt = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = nxt;
+            }
+            return prev;
+        }
 
     }
 
@@ -47,7 +59,24 @@ public class _206 {
     static class _3rd {
         public static void main(String[] args) {
             _3rd handler = new _3rd();
+
+            ListNode head = ListNodeIOUtils.transform("[1,2,3,4,5]");
+            handler.reverseList(head);
         }
+
+
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) return head;
+            ListNode newNode = reverseList(head.next);
+            //如果链表是 1->2->3->4->5，那么此时的cur就是5
+            //而head是4，head的next是5，next.next是空
+            //所以head.next.next 就是5->4
+            head.next.next = head;
+            //head的next需要断开
+            head.next = null;
+            return newNode;
+        }
+
     }
 
     static class _4th {
---
+++ src/main/java/com/frankcooper/io/ListNodeIOUtils.java
@@ -0,0 +1,81 @@
+package com.frankcooper.io;
+
+import com.frankcooper.struct.ListNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ListNodeIOUtils {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode node = Wrapper.stringToListNode(line);
+            Wrapper.prettyPrintLinkedList(node);
+        }
+    }
+
+
+    public static ListNode transform(String arrStr) {
+        ListNode head = Wrapper.stringToListNode(arrStr);
+        Wrapper.prettyPrintLinkedList(head);
+        return head;
+    }
+
+}
+
+class Wrapper {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static void prettyPrintLinkedList(ListNode node) {
+        while (node != null && node.next != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+
+        if (node != null) {
+            System.out.println(node.val);
+        } else {
+            System.out.println("Empty LinkedList");
+        }
+    }
+}
