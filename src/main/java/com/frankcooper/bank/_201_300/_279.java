8708d350835ff9ed211a6cca7d73aa407b652ae0
wat1r <cnwangzhou@hotmail.com>
2021-09-16 22:36:51 +0800

    daily

--- src/main/java/com/frankcooper/bank/_201_300/_279.java
+++ src/main/java/com/frankcooper/bank/_201_300/_279.java
@@ -1,7 +1,9 @@
 package com.frankcooper.bank._201_300;
 
-/*import java.util.*;
-import org.junit.Assert;*/
+import java.util.*;
+
+import org.junit.Assert;
+
 public class _279 {
 
     static class _1st {
@@ -10,8 +12,46 @@ public class _279 {
 
         }
 
+        public int numSquares(int n) {
+            List<Integer> squares = generateSquares(n);
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[] marked = new boolean[n + 1];
+            queue.add(n);
+            marked[n] = true;
+            int level = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                level++;
+                while (size-- > 0) {
+                    int cur = queue.poll();
+                    for (int square : squares) {
+                        int next = cur - square;
+                        if (next < 0) {
+                            break;
+                        }
+                        if (next == 0) {
+                            return level;
+                        }
+                        if (marked[next]) {
+                            continue;
+                        }
+                        marked[next] = true;
+                        queue.add(next);
+                    }
+                }
+            }
+            return -1;
+        }
 
 
+        private List<Integer> generateSquares(int n) {
+            List<Integer> squares = new ArrayList<>();
+            int base = (int) Math.sqrt(n);
+            for (int i = 1; i <= base; i++) {
+                squares.add((int) Math.pow(i, 2));
+            }
+            return squares;
+        }
 
 
     }
@@ -19,6 +59,32 @@ public class _279 {
     static class _2nd {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
+            handler.numSquares(13);
+        }
+
+        public int numSquares(int n) {
+            Queue<Integer> q = new LinkedList<>();
+            Set<Integer> vis = new HashSet<>();
+            q.offer(0);
+            vis.add(0);
+            int level = 0;
+            while (!q.isEmpty()) {
+                int size = q.size();
+                level++;
+                while (size-- > 0) {
+                    int cur = q.poll();
+                    for (int i = 1; i * i <= n; i++) {
+                        int next = cur + i * i;
+                        if (next == n) return level;
+                        if (next > n) break;
+                        if (!vis.contains(next)) {
+                            vis.add(next);
+                            q.offer(next);
+                        }
+                    }
+                }
+            }
+            return level;
         }
     }
 
