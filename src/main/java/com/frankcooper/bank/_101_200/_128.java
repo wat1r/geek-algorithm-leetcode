d64d4d9704e97300dca683193402649df3fd4f5e
wat1r <cnwangzhou@hotmail.com>
2021-09-17 09:56:04 +0800

    【重温经典】 完全平方数

--- src/main/java/com/frankcooper/bank/_101_200/_128.java
+++ src/main/java/com/frankcooper/bank/_101_200/_128.java
@@ -71,4 +71,27 @@ public class _128 {
             return ans;
         }
     }
+
+    static class _2nd {
+        public static void main(String[] args) {
+
+        }
+
+        public int longestConsecutive(int[] nums) {
+            if (nums == null || nums.length == 0) return 0;
+            Arrays.sort(nums);
+            int n = nums.length, res = 1, cur = 1;
+            for (int i = 0; i < n - 1; i++) {
+                if (nums[i + 1] != nums[i]) {
+                    if (nums[i + 1] == nums[i] + 1) {
+                        cur++;
+                    } else {
+                        res = Math.max(res, cur);
+                        cur = 1;
+                    }
+                }
+            }
+            return Math.max(res, cur);
+        }
+    }
 }
