a0d46a78e1ae378f09a4680a7055b9f7040da8a4
wat1r <cnwangzhou@hotmail.com>
2021-09-21 07:45:21 +0800

    零钱兑换

--- src/main/java/com/frankcooper/bank/_201_300/_300.java
+++ src/main/java/com/frankcooper/bank/_201_300/_300.java
@@ -102,4 +102,28 @@ public class _300 {
     }
 
 
+    static class _2nd {
+        public static void main(String[] args) {
+
+        }
+
+
+        public int lengthOfLIS(int[] nums) {
+            if (nums == null || nums.length == 0) return 0;
+            int n = nums.length;
+            int[] f = new int[n];
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                f[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        f[i] = Math.max(f[i], f[j] + 1);
+                    }
+                }
+                res = Math.max(res, f[i]);
+            }
+            return res;
+        }
+
+    }
 }
