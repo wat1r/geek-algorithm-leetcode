d52aa0926980ec6e1934ab150028272422659534
wangzhou <wangzhou@shengqugames.com>
2021-09-27 16:39:53 +0800

    1.daily

--- src/main/java/com/frankcooper/binary_tree/BinaryTree.java
+++ src/main/java/com/frankcooper/binary_tree/BinaryTree.java
@@ -4,6 +4,9 @@ import com.frankcooper.io.TreeNodeIOUtils;
 import com.frankcooper.struct.TreeNode;
 import org.junit.Assert;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author: wangzhou(Frank Cooper)
  * @date: 2021/9/27 14:20
@@ -15,12 +18,56 @@ public class BinaryTree {
         public static void main(String[] args) {
             _1st handler = new _1st();
             TreeNode root = TreeNodeIOUtils.transform("[1,2,3,4,5,null,null]");
-            Assert.assertEquals(2, handler.maxDepth(root));
+            Assert.assertEquals(3, handler.maxDepth(root));
         }
 
 
         public int maxDepth(TreeNode root) {
-            return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right) + 1);
+            return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+            TreeNode root = TreeNodeIOUtils.transform("[1,2,3,4,5,null,null]");
+            Assert.assertEquals(3, handler.maxDepth(root));
+        }
+
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            int depth = 0;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+            q.offer(null);
+            while (!q.isEmpty()) {
+                TreeNode cur = q.poll();
+                if (cur == null) depth++;
+                if (cur != null) {
+                    if (cur.left != null) q.offer(cur.left);
+                    if (cur.right != null) q.offer(cur.right);
+                } else if (!q.isEmpty()) q.offer(null);
+            }
+            return depth;
+        }
+    }
+
+    static class _3rd {
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            int level = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = queue.poll();
+                    if (curr.left != null) queue.offer(curr.left);
+                    if (curr.right != null) queue.offer(curr.right);
+                }
+                level++;
+            }
+            return level;
         }
     }
 }
