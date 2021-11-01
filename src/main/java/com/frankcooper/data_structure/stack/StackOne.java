beb4e217e72b1332d691538b769c9c37f4907447
wat1r <cnwangzhou@hotmail.com>
2021-11-01 21:37:36 +0800

    Stack

--- src/main/java/com/frankcooper/data_structure/stack/StackOne.java
+++ src/main/java/com/frankcooper/data_structure/stack/StackOne.java
@@ -103,6 +103,87 @@ public class StackOne {
     static class _3rd {
         public static void main(String[] args) {
             _3rd handler = new _3rd();
+            handler.test();
+        }
+
+        private void test() {
+            MyStack myStack = new MyStack();
+            myStack.push(11);
+            myStack.push(22);
+            myStack.push(33);
+            myStack.push(44);
+            myStack.push(55);
+            myStack.push(66);
+            myStack.push(77);
+            System.out.println("Item popped is " + myStack.pop());
+            System.out.println("Item popped is " + myStack.pop());
+            System.out.println("Middle Element is " + myStack.findMiddle());
+        }
+
+
+        class DLLNode {
+            private DLLNode prev;
+            private DLLNode next;
+            private int data;
+
+            public DLLNode(int data) {
+                this.data = data;
+            }
+        }
+
+        class MyStack {
+            private DLLNode head;
+            private DLLNode mid;
+            private int count;
+
+            public MyStack() {
+            }
+
+            public void push(int data) {
+                DLLNode node = new DLLNode(data);
+                node.prev = null;
+                node.next = head;
+                count++;
+                if (count == 1) {
+                    mid = node;
+                } else {
+                    head.prev = node;
+                    if (count % 2 != 0) {//odd 奇数
+                        mid = mid.prev;
+                    }
+                }
+                head = node;
+            }
+
+            public int pop() {
+                if (count == 0) {
+                    System.out.println("Stack is empty!");
+                    return -1;
+                }
+                int item = head.data;
+                head = head.next;
+                if (head != null) {
+                    head.prev = null;
+                }
+                count--;
+                if (count % 2 == 0) {
+                    mid = mid.next;
+                }
+                return item;
+            }
+
+
+            public int findMiddle() {
+                if (count == 0) {
+                    System.out.println("Stack is empty!");
+                    return -1;
+                }
+                return mid.data;
+            }
+
+
+
+
         }
     }
 
