8fa3c74c15e7a1758245288dd6a04485ef37a073
wat1r <cnwangzhou@hotmail.com>
2021-11-08 19:53:24 +0800

    二分

--- src/main/java/com/frankcooper/bank/_101_200/_162.java
+++ src/main/java/com/frankcooper/bank/_101_200/_162.java
@@ -33,6 +33,19 @@ public class _162 {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
         }
+
+        public int findPeakElement(int[] nums) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] > nums[mid + 1]) {//与mid右侧挨着的点进行比较筛选
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return l;
+        }
     }
 
 
