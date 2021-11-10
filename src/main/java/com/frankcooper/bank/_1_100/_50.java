4e22af8d62f7ef444993dd768e843e2e2f30c2fb
wat1r <cnwangzhou@hotmail.com>
2021-11-10 20:43:56 +0800

    二分

--- src/main/java/com/frankcooper/bank/_1_100/_50.java
+++ src/main/java/com/frankcooper/bank/_1_100/_50.java
@@ -100,4 +100,79 @@ public class _50 {
         }
 
     }
+
+    static class _4th_1 {
+        public static void main(String[] args) {
+
+        }
+
+        public double myPow(double x, int n) {
+            long N = n;
+            if (N < 0) {
+                x = 1 / x;
+                N = -N;
+            }
+            double res = 1.0;
+            while (N != 0) {
+                if ((N & 1) == 1) res *= x;
+                x *= x;
+                N >>= 1;
+            }
+            return res;
+        }
+    }
+
+    static class _4th_2 {
+        public double myPow(double x, int n) {
+            double res = 1.0;
+            for (int i = n; i != 0; i /= 2) {
+                if ((i & 1) == 1) {
+                    res *= x;
+                }
+                x *= x;
+            }
+            return n < 0 ? 1 / res : res;
+        }
+    }
+
+    static class _4th_3 {
+        //注意 整数溢出 <0 的处理方式
+
+        /**
+         * When n is negative, we are reducing it value by one and then converting to positive integer.
+         * e.g. n = -2 is converted into n = 1
+         * Similarly, n = -2147483648 is converted to n = 2147483647
+         * <p>
+         * Since we reduced value by one we are accommodating that at the end by multiplying it with 1/x.
+         *
+         * @param x
+         * @param n
+         * @return
+         */
+        public double myPow(double x, int n) {
+            if (n == 0) return 1.0;
+            if (n < 0) {
+                return 1 / x * myPow(1 / x, -(n + 1));
+            }
+            return ((n % 2) == 1) ? x * myPow(x * x, n / 2) : myPow(x * x, n / 2);
+        }
+
+
+    }
+
+    static class _4th_4 {
+        public double myPow(double x, int n) {
+            if (n == 0) return 1;
+            if (n == Integer.MIN_VALUE) {
+                x = x * x;
+                n = n / 2;
+            }
+            if (n < 0) {
+                n = -n;
+                x = 1 / x;
+            }
+
+            return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+        }
+    }
 }
