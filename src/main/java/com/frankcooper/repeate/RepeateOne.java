1b5e5c8a6b1c6a18d7729e40fbcdbdbe41f3c0fe
wat1r <cnwangzhou@hotmail.com>
2021-12-29 22:51:05 +0800

    vim

--- src/main/java/com/frankcooper/repeate/RepeateOne.java
+++ src/main/java/com/frankcooper/repeate/RepeateOne.java
@@ -203,10 +203,10 @@ public class RepeateOne {
         }
 
         public int sizeOfBinaryTree(TreeNode root) {
-            if (root == null) return 0;
-            int l = sizeOfBinaryTree(root.left);
-            int r = sizeOfBinaryTree(root.right);
-            return l + r + 1;
-        }
+		    if (root == null) return 0;
+		    int l = sizeOfBinaryTree(root.left);
+		    int r = sizeOfBinaryTree(root.right);
+		    return l + r + 1;
+	}
     }
 }
