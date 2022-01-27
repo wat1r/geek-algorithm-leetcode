c4f6136c165db7ea5fd3ed9792f41a00fd01ef10
wangzhou <wangzhou@shengqugames.com>
2022-01-27 15:09:21 +0800

    1.daily

---
+++ src/main/java/com/frankcooper/bank/_1501_2000/_2022.java
@@ -0,0 +1,45 @@
+package com.frankcooper.bank._1501_2000;
+
+import java.util.*;
+
+import org.junit.Assert;
+
+public class _2022 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+
+        }
+
+        public int[][] construct2DArray(int[] original, int m, int n) {
+            if (original.length != m * n) return new int[][]{};
+            int[][] arr = new int[m][n];
+            for (int i = 0; i < original.length; i++) {
+                int r = i / n, c = i % n;
+                arr[r][c] = original[i];
+            }
+            return arr;
+        }
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
