dc7d7bae12b6c5766fb29f5cbabb3b58c962d824
wat1r <cnwangzhou@hotmail.com>
2022-02-20 16:01:38 +0800

    Flink

--- src/main/java/com/frankcooper/bank/_1_100/_15.java
+++ src/main/java/com/frankcooper/bank/_1_100/_15.java
@@ -80,4 +80,35 @@ public class _15 {
             return res;
         }
     }
+
+
+    static class _2nd {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (nums == null || nums.length <= 2) return res;
+            Arrays.sort(nums);
+            int n = nums.length;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] > 0) break;
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                int l = i + 1, r = n - 1;
+                while (l < r) {
+                    int t = nums[i] + nums[l] + nums[r];
+                    if (t == 0) {
+                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (t < 0) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+            return res;
+        }
+
+    }
 }
