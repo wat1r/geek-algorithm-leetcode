cbb14319cc97edef0a114ae95ece9f76f6078513
wat1r <cnwangzhou@hotmail.com>
2022-03-02 18:59:41 +0800

    daily

--- src/main/java/com/frankcooper/bank/_701_1000/_977.java
+++ src/main/java/com/frankcooper/bank/_701_1000/_977.java
@@ -12,4 +12,16 @@ public class _977 {
         Arrays.sort(res);
         return res;
     }
+
+    static class _1st {
+        public int[] sortedSquares(int[] nums) {
+            int n = nums.length;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] < 0) nums[i] = -nums[i];
+            }
+            Arrays.sort(nums);
+            for (int i = 0; i < n; i++) nums[i] *= nums[i];
+            return nums;
+        }
+    }
 }
