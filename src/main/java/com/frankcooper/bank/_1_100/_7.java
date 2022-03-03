9d460d84afe2713df2747e2ec18b9f853dc0b370
wangzhou <wangzhou@shengqugames.com>
2022-03-03 19:40:39 +0800

    1.gitbook

--- src/main/java/com/frankcooper/bank/_1_100/_7.java
+++ src/main/java/com/frankcooper/bank/_1_100/_7.java
@@ -1,9 +1,5 @@
 package com.frankcooper.bank._1_100;
 
-import java.util.*;
-
-import org.junit.Assert;
-
 public class _7 {
 
     static class _1st {
@@ -26,7 +22,6 @@ public class _7 {
                 x = -x;
             }
             long res = 0;
-            int bit = 0;
             while (x > 0) {
                 res = res * 10 + x % 10;
                 x /= 10;
@@ -40,6 +35,25 @@ public class _7 {
     static class _2nd {
         public static void main(String[] args) {
             _2nd handler = new _2nd();
+            System.out.println(Integer.MIN_VALUE);
+            System.out.println(Integer.MAX_VALUE);
+            int x = -2147483648;
+            handler.reverse(x);
+        }
+
+        public int reverse(int x) {
+            int sign = 1;
+            if (x < 0) {
+                x = -x;
+                sign = -1;
+            }
+            long res = 0;
+            while (x > 0) {
+                res = res * 10 + x % 10;
+                x /= 10;
+                if (res >= Integer.MAX_VALUE) return 0;
+            }
+            return (int) res * sign;
         }
     }
 
@@ -48,6 +62,9 @@ public class _7 {
         public static void main(String[] args) {
             _3rd handler = new _3rd();
         }
+
+
+
     }
 
     static class _4th {
---
+++ src/main/java/com/frankcooper/other/BonusUtils.java
@@ -0,0 +1,23 @@
+package com.frankcooper.other;
+
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2022/3/3 16:53
+ * @description:
+ */
+public class BonusUtils {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    static class BonusContext{
+//        private double
+    }
+
+
+}
