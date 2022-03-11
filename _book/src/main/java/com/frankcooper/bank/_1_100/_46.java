578fec52624f2e78f67412ae5203f6539b3e7a8b
wat1r <cnwangzhou@hotmail.com>
2022-03-11 10:00:52 +0800

    deploy

--- _book/src/main/java/com/frankcooper/bank/_1_100/_46.java
+++ _book/src/main/java/com/frankcooper/bank/_1_100/_46.java
@@ -140,4 +140,35 @@ public class _46 {
         }
     }
 
+    static class _4th {
+        List<List<Integer>> resList = new ArrayList<>();
+
+        public List<List<Integer>> permute(int[] nums) {
+
+            if (nums == null || nums.length == 0) return resList;
+            List<Integer> levelList = new ArrayList<>();
+            int n = nums.length;
+            for (int num : nums) levelList.add(num);
+            backtrace(levelList, n, 0);
+            return resList;
+        }
+
+        /**
+         * @param levelList 当前的结果列表
+         * @param n         nums的length
+         * @param index     当前处理到idx
+         */
+        private void backtrace(List<Integer> levelList, int n, int index) {
+            if (index == n) {
+                resList.add(new ArrayList<>(levelList));
+                return;
+            }
+            for (int i = index; i < n; i++) {
+                Collections.swap(levelList, index, i);
+                backtrace(levelList, n, index + 1);
+                Collections.swap(levelList, index, i);
+            }
+        }
+    }
+
 }
