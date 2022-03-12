efd8eb275dedb8508a64d4842aa5a4bccd946fb7
wat1r <cnwangzhou@hotmail.com>
2022-03-12 22:46:07 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ 
+echo "=========git pull======="
+git pull
+
+echo "=========gitbook build=======" 
+gitbook  build
+
+echo "=========deploy======="
+git add .  &&  git commit -m "deploy" && git push
+
+#echo "=========git serve======="
+#gitbook serve
