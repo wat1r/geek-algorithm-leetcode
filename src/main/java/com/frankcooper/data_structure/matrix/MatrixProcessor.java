3cb2ce7403ffaafa1da0583e663c46aefd19b5c8
wat1r <cnwangzhou@hotmail.com>
2022-03-23 17:34:19 +0800

    deploy

--- src/main/java/com/frankcooper/data_structure/matrix/MatrixProcessor.java
+++ src/main/java/com/frankcooper/data_structure/matrix/MatrixProcessor.java
@@ -47,4 +47,45 @@ public class MatrixProcessor {
         }
 
     }
+
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+            int[][] matrix = {
+                    {1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12},
+                    {13, 14, 15, 16}
+            };
+            int N = matrix.length;
+            handler.display(matrix, N);
+            handler.rotateMatrix(matrix, N);
+            handler.display(matrix, N);
+        }
+
+
+        private void rotateMatrix(int[][] matrix, int N) {
+            //考虑每一个环
+            for (int x = 0; x < N / 2; x++) {
+                for (int y = x; y < N - x - 1; y++) {
+                    int t = matrix[x][y];
+                    matrix[x][y] = matrix[y][N - 1 - x];
+                    matrix[y][N - 1 - x] = matrix[N - 1 - x][N - 1 - y];
+                    matrix[N - 1 - x][N - 1 - y] = matrix[N - 1 - y][x];
+                    matrix[N - 1 - y][x] = t;
+                }
+            }
+        }
+
+
+        private void display(int[][] matrix, int N) {
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    System.out.printf("%d\t", matrix[r][c]);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
 }
