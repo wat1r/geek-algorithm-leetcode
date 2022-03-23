8e746de2e4ff53aaa191313723896cb8de1d88b3
wat1r <cnwangzhou@hotmail.com>
2022-03-23 12:48:21 +0800

    deploy

--- docs/leetcode/classify/string.md
+++ docs/leetcode/classify/string.md
@@ -1,5 +1,39 @@
 > 
 
+## [6. Z 字形变换](https://leetcode-cn.com/problems/zigzag-conversion/)
+
+```java
+public String convert(String s, int numRows) {
+    //r表示每一行的索引 [0,numRows-1]
+    //flag是 1 或者 -1 按从上到下或者从下到上 两个方向区分正负
+    int r = 0, flag = 1;
+    //结果列表
+    StringBuilder[] sb = new StringBuilder[numRows];
+    for (int k = 0; k < numRows; k++) sb[k] = new StringBuilder();
+    int i = 0;
+    while (i < s.length()) {
+        if (r == numRows) {
+            flag = -flag;
+            //区分nowrows 是否为1
+            if (numRows == 1) r -= 1;
+            else r -= 2;
+        } else if (r == -1) {
+            flag = -flag;
+            if(numRows == 1) r+=1;
+            else r+=2;
+        }
+
+        sb[r].append(s.charAt(i++));
+        r += flag;
+    }
+    StringBuilder ans = new StringBuilder();
+    for (int k = 0; k < numRows; k++) ans.append(sb[k]);
+    return ans.toString();
+}
+```
+
+
+
 ## [8. 字符串转换整数 (atoi)](https://leetcode-cn.com/problems/string-to-integer-atoi/)
 
 ```java
--- src/main/java/com/frankcooper/bank/_1_100/_6.java
+++ src/main/java/com/frankcooper/bank/_1_100/_6.java
@@ -15,22 +15,26 @@ public class _6 {
             String s = "PAYPALISHIRING";
             int numRows = 3;
 //            handler.convert(s, numRows);
-//            Assert.assertEquals(handler.convert(s, numRows), "PAHNAPLSIIGYIR");
-            s = "AB";
-            numRows = 1;
-            Assert.assertEquals(handler.convert(s, numRows), "AB");
+            Assert.assertEquals(handler.convert(s, numRows), "PAHNAPLSIIGYIR");
+//            s = "AB";
+//            numRows = 1;
+//            Assert.assertEquals(handler.convert(s, numRows), "AB");
 
         }
 
 
         public String convert(String s, int numRows) {
+            //r表示每一行的索引 [0,numRows-1]
+            //flag是 1 或者 -1 按从上到下或者从下到上 两个方向区分正负
             int r = 0, flag = 1;
+            //结果列表
             StringBuilder[] sb = new StringBuilder[numRows];
             for (int k = 0; k < numRows; k++) sb[k] = new StringBuilder();
             int i = 0;
             while (i < s.length()) {
                 if (r == numRows) {
                     flag = -flag;
+                    //区分nowrows 是否为1
                     if (numRows == 1) r -= 1;
                     else r -= 2;
                 } else if (r == -1) {
