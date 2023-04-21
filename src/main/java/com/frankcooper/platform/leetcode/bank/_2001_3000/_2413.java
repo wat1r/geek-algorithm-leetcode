e65177db2dd4d077ec7b5c376c30eca91ad15a2f
wangzhou <wangzhou@shengqugames.com>
2023-04-21 09:27:43 +0800

    daily

---
+++ src/main/java/com/frankcooper/platform/leetcode/bank/_2001_3000/_2413.java
@@ -0,0 +1,38 @@
+package com.frankcooper.platform.leetcode.bank._2001_3000;
+
+/*import java.util.*;
+import org.junit.Assert;*/
+public class _2413 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+
+        }
+
+        public int smallestEvenMultiple(int n) {
+            return (n % 2 + 1) * n;
+        }
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
