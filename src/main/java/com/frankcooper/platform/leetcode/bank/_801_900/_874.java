b7ebee006336c8a175b331ee1007480388968788
wangzhou <wangzhou@shengqugames.com>
2023-07-19 19:41:50 +0800

    1.daily
    Signed-off-by: wangzhou <wangzhou@shengqugames.com>

---
+++ src/main/java/com/frankcooper/platform/leetcode/bank/_801_900/_874.java
@@ -0,0 +1,42 @@
+package com.frankcooper.platform.leetcode.bank._801_900;
+
+/*import java.util.*;
+import org.junit.Assert;*/
+public class _874 {
+
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+
+
+        }
+
+//        public int robotSim(int[] commands, int[][] obstacles) {
+//            int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};//北东南西
+//            int n = commands.length;
+//
+//
+//        }
+
+
+    }
+
+    static class _2nd {
+        public static void main(String[] args) {
+            _2nd handler = new _2nd();
+        }
+    }
+
+
+    static class _3rd {
+        public static void main(String[] args) {
+            _3rd handler = new _3rd();
+        }
+    }
+
+    static class _4th {
+        public static void main(String[] args) {
+            _4th handler = new _4th();
+        }
+    }
+}
