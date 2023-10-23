7a5f38942b5275b109d7bb59444ad0bc9ec63d0c
wangzhou <wangzhou@shengqugames.com>
2023-10-23 20:03:40 +0800

    1.daily
    Signed-off-by: wangzhou <wangzhou@shengqugames.com>

--- src/main/java/com/frankcooper/platform/leetcode/bank/_2001_3000/_2316.java
+++ src/main/java/com/frankcooper/platform/leetcode/bank/_2001_3000/_2316.java
@@ -113,7 +113,48 @@ public class _2316 {
     static class _3rd {
         public static void main(String[] args) {
             _3rd handler = new _3rd();
+            int n = 7;
+            int[][] edges = {{0, 2}, {0, 5}, {2, 4}, {1, 6}, {5, 4}};
+            Assert.assertEquals(14, handler.countPairs(n, edges));
+
         }
+
+        public long countPairs(int n, int[][] edges) {
+            List<Integer>[] g = new ArrayList[n];
+            Arrays.setAll(g, e -> new ArrayList<>());
+            for (int[] e : edges) {
+                int x = e[0], y = e[1];
+                g[x].add(y);
+                g[y].add(x); // 建图
+            }
+
+            boolean[] vis = new boolean[n];
+            long ans = 0;
+            for (int i = 0, total = 0; i < n; i++) {
+                if (!vis[i]) { // 未访问的点：说明找到了一个新的连通块
+                    int size = dfs(i, g, vis);
+                    ans += (long) size * total;
+                    total += size;
+                }
+            }
+            return ans;
+        }
+
+        private int dfs(int x, List<Integer>[] g, boolean[] vis) {
+            vis[x] = true; // 避免重复访问同一个点
+            int size = 1;
+            for (int y : g[x]) {
+                if (!vis[y]) {
+                    size += dfs(y, g, vis);
+                }
+            }
+            return size;
+        }
+//
+//        作者：灵茶山艾府
+//        链接：https://leetcode.cn/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
     }
 
     static class _4th {
